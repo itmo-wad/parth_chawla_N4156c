19cc2d9cadcc7ab376742a25a5efbc7e9173414a
PARTH CHAWLA <82279033+sponge-bobs-square-pants@users.noreply.github.com>
2021-04-11 13:57:45 +0530

    Add files via upload

---
+++ task_1/modal_window.js
@@ -0,0 +1,94 @@
+var modal_window = document.getElementById('modol1');
+        var modalImg = document.getElementById("img1");
+        var image = document.getElementById("image1");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();  /* blur not working..why? */
+           /* modal_window.style.display = "flex";*/
+        }
+var modal_window = document.getElementById('modol2');
+        var modalImg = document.getElementById("img2");
+        var image = document.getElementById("image2");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();
+        }
+var modal_window = document.getElementById('modol3');
+        var modalImg = document.getElementById("img3");
+        var image = document.getElementById("image3");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();
+        }
+
+
+
+/*4-6 */
+var modal_window = document.getElementById('modol4');
+        var modalImg = document.getElementById("img4");
+        var image = document.getElementById("image4");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();  /* blur not working..why? */
+            modal_window.style.display = "cover";
+        }
+var modal_window = document.getElementById('modol5');
+        var modalImg = document.getElementById("img5");
+        var image = document.getElementById("image5");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();  /* blur not working..why? */
+            modal_window.style.display = "cover";
+        }
+var modal_window = document.getElementById('modol6');
+        var modalImg = document.getElementById("img6");
+        var image = document.getElementById("image6");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();  /* blur not working..why? */
+            modal_window.style.display = "cover";
+        }
+
+/*7-9 */
+var modal_window = document.getElementById('modol7');
+        var modalImg = document.getElementById("img7");
+        var image = document.getElementById("image7");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();  /* blur not working..why? */
+            modal_window.style.display = "cover";
+        }
+var modal_window = document.getElementById('modol8');
+        var modalImg = document.getElementById("img8");
+        var image = document.getElementById("image8");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();  /* blur not working..why? */
+            modal_window.style.display = "cover";
+        }
+var modal_window = document.getElementById('modol9');
+        var modalImg = document.getElementById("img9");
+        var image = document.getElementById("image9");
+        
+        image.onclick = function(){
+            modal_window.style.display = "block";
+            modalImg.src = this.src;
+            document.getElementById('pol').blur();  /* blur not working..why? */
+            modal_window.style.display = "cover";
+        }
