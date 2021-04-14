7471eb1a8abe53165d4f7cac8538c7e6387d3850
PARTH CHAWLA <82279033+sponge-bobs-square-pants@users.noreply.github.com>
2021-04-14 10:20:18 +0530

    Add files via upload

--- task_2/main.py
+++ task_2/main.py
@@ -1,1 +1,31 @@
+from flask import Flask, render_template, url_for, send_from_directory
 
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route('/misc/<path:path>')
+def index(path):
+    return app.send_static_file(path)
+
+
+@app.route('/img/<path:path>')
+def index2(path):
+    return send_from_directory('static\\img', path)
+
+
+@app.route('/static/<path:path>')
+def index3(path):
+    return app.send_static_file(path)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
